116c23fe30e6e864f61c237f3fc0f29de6a30c7c
cleona <cleonatan1998@gmail.com>
2021-05-07 12:37:08 +0800

    callAPI

--- src/components.js/TransactionInfo.js
+++ src/components.js/TransactionInfo.js
@@ -13,16 +13,54 @@ class TransactionInfo extends Component {
              date: '',
              amount: '',
              eGift: '',
-             message: ''
+             message: '',
+             expenseCat: ''
         }
     }
     
-    render() {
-        return (
-            <div>
-                <ShowTransaction/>
-            </div>
-        )
+
+    componentDidMount(){
+        this.callAPI()
+    } 
+
+    //call API 
+    callAPI(){
+        var axios = require('axios');
+        var data = JSON.stringify({
+        "custID": 21,
+        "accountKey": "v8f9qys7-t3ru-81jm-pqb4-7qngr7rna5i"
+        });
+
+        var config = {
+        method: 'post',
+        url: 'https://ipllrj2mq8.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view',
+        headers: { 
+            'x-api-key': 'PgfXlfXJFM2QyTmuBOTKUazP03JWex648svPUCl5', 
+            'Content-Type': 'application/json'
+        },
+        data : data
+        };
+
+        axios(config)
+        .then(function (response) {
+            this.setState({
+                info = response.data
+            })
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+
+        render() 
+            const {info} = this.state;
+            return (
+                <div>
+                    <h1>Transaction Details</h1>
+                    <ShowTransaction/>
+                </div>
+            )
+        };
     }
 }
 
